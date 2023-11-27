b41352c66acd0cd89fe12310bc821c986807a4ee
Neel Nirajkumar Shah <nshah109@asu.edu>
2023-11-27 13:08:53 -0700

    updated implementation

--- client/index.js
+++ client/index.js
@@ -1,5 +1,21 @@
+function addSymptom() {
+    const symptomsContainer = document.getElementById('symptoms-container');
+    
+    const newSymptomInput = document.createElement('div');
+    newSymptomInput.classList.add('symptom-input');
+    
+    newSymptomInput.innerHTML = `
+        <label for="symptom">Enter Symptom:</label>
+        <input type="text" name="symptom" class="symptom">
+        <button onclick="addSymptom()">+</button>
+    `;
+    
+    symptomsContainer.appendChild(newSymptomInput);
+}
+
 function submitSymptoms() {
-    const symptomsInput = document.getElementById('symptoms').value;
+    const symptomInputs = document.querySelectorAll('.symptom');
+    const symptoms = Array.from(symptomInputs).map(input => input.value);
     
     // Sending data to the backend
     fetch('http://localhost:3000/submitSymptoms', {
@@ -7,7 +23,7 @@ function submitSymptoms() {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ symptoms: symptomsInput }),
+        body: JSON.stringify({ symptoms }),
     })
     .then(response => response.json())
     .then(data => console.log(data))
